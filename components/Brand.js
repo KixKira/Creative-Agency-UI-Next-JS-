8e9af5f9a042c023b81225cb84da1634d1c5385e
Oscar Machado <oemqkix@gmail.com>
2026-01-29 16:32:43 -0400

    feat: integrate Swiper for responsive brand carousel in Brand component

--- components/Brand.js
+++ components/Brand.js
@@ -1,6 +1,9 @@
 import React from "react";
 import { Title } from "./common/Title";
 import { brand } from "@/assets/data/dummydata";
+import { Swiper, SwiperSlide } from "swiper/react";
+import { Autoplay } from "swiper/modules";
+import "swiper/css";
 
 const Brand = () => {
   return (
@@ -9,17 +12,58 @@ const Brand = () => {
         <div className="heading-title">
           <Title title="JU WE TRUST: Marcas que han trabajado con nosotros" />
         </div>
-        <div className="brand-content grid-6 py">
-          {brand.map((item) => (
-            <div className="images" key={item.id}>
-              <img
-                src={item.cover}
-                alt={item.id}
-                width="100px"
-                height="100px"
-              />
-            </div>
-          ))}
+        <div className="brand-content py">
+          <Swiper
+            modules={[Autoplay]}
+            spaceBetween={30}
+            slidesPerView={4}
+            loop={true}
+            autoplay={{
+              delay: 2500,
+              disableOnInteraction: false,
+              pauseOnMouseEnter: true,
+            }}
+            breakpoints={{
+              480: {
+                slidesPerView: 3,
+                spaceBetween: 30,
+              },
+              768: {
+                slidesPerView: 4,
+                spaceBetween: 40,
+              },
+              1024: {
+                slidesPerView: 5,
+                spaceBetween: 50,
+              },
+              1280: {
+                slidesPerView: 6,
+                spaceBetween: 60,
+              },
+            }}
+            className="brand-slider"
+          >
+            {brand.map((item) => (
+              <SwiperSlide key={item.id}>
+                <div className="images">
+                  <img
+                    src={item.cover}
+                    alt={`Brand ${item.id}`}
+                    width="100"
+                    height="100"
+                    style={{
+                      width: "100%",
+                      height: "auto",
+                      objectFit: "contain",
+                      maxWidth: "150px",
+                      margin: "0 auto",
+                      display: "block",
+                    }}
+                  />
+                </div>
+              </SwiperSlide>
+            ))}
+          </Swiper>
         </div>
       </div>
     </section>
